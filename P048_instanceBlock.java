5b65820d9368a0baef2dee9a732989da9e87b99d
Pradeep yadav <126454840+mahigabru@users.noreply.github.com>
2023-02-27 14:44:53 +0530

    Add files via upload

---
+++ P048_instanceBlock.java
@@ -0,0 +1,9 @@
+class P048_instanceblock {
+    {//statics block
+        System.out.println("hello world");
+    }
+    public static void main(String[] args) {
+        P048_instanceblock obj=new P048_instanceblock();
+    }
+}
+///***isme bina object banaye koi output nhi milta */
