c63e5cb736fb9ed09bc865910b5b5390545d2ee2
Pradeep yadav <126454840+mahigabru@users.noreply.github.com>
2023-02-27 14:50:23 +0530

    Add files via upload

---
+++ P054_simple_inheritance.java
@@ -0,0 +1,16 @@
+
+
+                           // simple inheritance//
+class supper{
+   public int a=10;
+    public int b=20;
+    
+}
+class sub extends supper{
+public static void main(String[] args) {
+    //sub obj=new sub();
+    System.out.println("the sum of the number is :"+(a+b));
+}
+}
+
+///*** agar super ke variable ko sub class me use krna hoga to ya to usko private kr denge nhi to usko static krke uska sub class ka object banana prega */
