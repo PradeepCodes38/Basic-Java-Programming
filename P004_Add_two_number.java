2fbf2da74fafac6705c19b2143584fd914c11781
Pradeep yadav <126454840+mahigabru@users.noreply.github.com>
2023-02-27 14:15:44 +0530

    Add files via upload

---
+++ P004_Add_two_number.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class P004_Qs {
+    public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+        System.out.print("enter the value of a:");
+        int a=sc.nextInt();
+        System.out.print("enter the value of b:");
+        int b=sc.nextInt();
+        System.out.println("the sum of a and b is" + (a+b));
+    }
+}
