945eeb4c296e02bd38a230e00fe90dbda75f46e5
Pradeep yadav <126454840+mahigabru@users.noreply.github.com>
2023-02-27 14:45:31 +0530

    Add files via upload

---
+++ P049_statciaBlock.java
@@ -0,0 +1,11 @@
+ class P049_statciaBlock {
+   static{//using static keyword in static block
+    System.out.println("hello mahi");
+   }
+   public static void main(String[] args) {
+    
+   } 
+}
+////*** isme object create krne ka koi jarurat nhi hota */
+
+////***isme koi v veriavle ko declare krne ke liye static keyword lagana prta hai */
