b898bc3839fda39632d899bb6909bed8173bd64c
Pradeep yadav <126454840+mahigabru@users.noreply.github.com>
2023-02-27 14:59:19 +0530

    Update P001 output.java

--- P001 output.java
+++ P001 output.java
@@ -1,5 +1,5 @@
 class output{
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world");//here println used for output
     }
 }
