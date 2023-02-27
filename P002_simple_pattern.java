e12fc536c2a4f38a62caa2853dc818db0d18cd37
Pradeep yadav <126454840+mahigabru@users.noreply.github.com>
2023-02-27 14:11:23 +0530

    Add files via upload

---
+++ P002_simple_pattern.java
@@ -0,0 +1,8 @@
+public class P002_Qs {
+    public static void main(String[] args) {
+    System.out.println("*");
+    System.out.println("**");
+    System.out.println("***");
+    System.out.println("****");
+    }
+}
