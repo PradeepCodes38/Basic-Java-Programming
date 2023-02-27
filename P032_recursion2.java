5af927821ddac7bf8a92088d1c90e9e7ef7d3830
Pradeep yadav <126454840+mahigabru@users.noreply.github.com>
2023-02-27 14:26:37 +0530

    Add files via upload

---
+++ P032_recursion2.java
@@ -0,0 +1,13 @@
+public class P032_recursion2 {
+    public static void printNum(int n) {
+        if(n==6){
+            return;
+        }
+        System.out.print(n+" ");
+        printNum(n+1);
+    }
+    public static void main(String[] args) {
+        int n=1;
+        printNum(n);
+    }
+}
