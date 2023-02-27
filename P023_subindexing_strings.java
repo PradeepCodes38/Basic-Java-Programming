1c6826e9cb6e174599cb3ca86fe4d339717cc935
Pradeep yadav <126454840+mahigabru@users.noreply.github.com>
2023-02-27 14:22:31 +0530

    Add files via upload

---
+++ P023_subindexing_strings.java
@@ -0,0 +1,7 @@
+public class P023_subindexing_strings {
+    public static void main(String[] args) {
+        String title="happy life is going on";
+        String sub=title.substring(6,title.length());
+        System.out.println(sub);
+    }
+}
