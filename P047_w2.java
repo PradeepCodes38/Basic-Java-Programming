9e1655123fddea14d2ff57639328bd32f3f2bbf8
Pradeep yadav <126454840+mahigabru@users.noreply.github.com>
2023-02-27 14:44:08 +0530

    Add files via upload

---
+++ P047_w2.java
@@ -0,0 +1,15 @@
+ class P047_2 {
+   P047_2()
+   {
+System.out.println("my roll no is");
+   } 
+   P047_2(int a)
+   {
+    //this();//if we not put this keyword then it will print only the value of a means 100 
+    System.out.print(a);
+   }
+   public static void main(String[] args) {
+    P047_2 obj=new P047_2(100);
+
+   }
+}
