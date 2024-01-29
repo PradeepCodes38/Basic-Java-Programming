e82bcdd98ed93f3a1fcf5b346752fb5c0e7bb261
Pradeep yadav <126454840+Pradeepyadav38@users.noreply.github.com>
2024-01-29 10:51:38 +0530

    P003_User_Input.java

--- P003_User_Input.java
+++ P003_User_Input.java
@@ -2,8 +2,15 @@ import java.util.Scanner;
 
 public class P003_Input {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        String name=sc.nextLine();
+        // Create a Scanner object to read user input from the console
+        Scanner sc = new Scanner(System.in);
+
+        // Prompt the user to enter a string and read the input
+        String name = sc.nextLine();
+
+        // Print the entered string to the console
         System.out.println(name);
+
     }
 }
+
