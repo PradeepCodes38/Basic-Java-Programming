db6b1e7d39574187f5f517054eb632169f07f40f
Pradeep yadav <126454840+Pradeepyadav38@users.noreply.github.com>
2024-01-29 10:50:30 +0530

    P002_simple_pattern.java

--- P002_simple_pattern.java
+++ P002_simple_pattern.java
@@ -1,8 +1,16 @@
 public class P002_Qs {
     public static void main(String[] args) {
-    System.out.println("*");
-    System.out.println("**");
-    System.out.println("***");
-    System.out.println("****");
+        // Print a line with one asterisk
+        System.out.println("*");
+        
+        // Print a line with two asterisks
+        System.out.println("**");
+        
+        // Print a line with three asterisks
+        System.out.println("***");
+        
+        // Print a line with four asterisks
+        System.out.println("****");
     }
 }
+
